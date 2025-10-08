a5d4538073de83065f432f5f9b2c51a77ca70bd9
Yadav <yatharth.yadav@capgemini.com>
2025-10-08 12:38:12 +0530

    first commit

---
+++ src/main/java/com/project/pet_eureka_server/PetEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.project.pet_eureka_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetEurekaServerApplication.class, args);
+	}
+
+}
